b05bbaf8890f547154b4c588cb01669162ad0ea7
Khanjan Marthak <khanjan.m@simformsolutions.com>
2022-03-04 19:15:56 +0530

    TE5-TE85, added bootstrap and controllers with views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     root "homepages#home"
     get "/home", to:"homepages#home"
+    get "/about", to: "abouts#aboutus"
+    get "/contactus", to: "contact_us#contactus"
   # Defines the root path route ("/")
   # root "articles#index"
 end

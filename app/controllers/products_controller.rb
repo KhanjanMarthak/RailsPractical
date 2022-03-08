fca904376c0b36f106307907f87de94e698b1649
Khanjan Marthak <khanjan.m@simformsolutions.com>
2022-03-08 20:04:51 +0530

    feat TE5-TE86, CRUD operation completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,44 @@
-class ProductsController < ApplicationController
+class ProductsController < ApplicationController 
+  def products
+    @products= Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new 
+    @product = Product.new
+  end
+
+  def create
+    puts params
+    @product= Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end 
+
+  def update
+    puts params
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path(@product)  
+  end
     
-    def products
-      @products= Product.all
-    end
+  def destroy
+    @product= Product.find(params[:id])
+    @product.destroy
+    redirect_to product_path(@product)
+  end
 
-    def show
-      @products = Product.all
-    end
+  private
+  def product_params
+    params.require(:product).permit(:product_name, :description, :team_size, :charges)
+  end
 
-    def new 
-      @product = Product.new
-    end
-
-    def create
-      puts params
-      @product= Product.new(product_params)
-      if @product.save
-        redirect_to product_path(@product)
-      end
-    end
-
-    private
-    def product_params
-      params.require(:product).permit(:product_name, :description, :team_size, :charges)
-    end
-    def update
-    
-    end
- 
 end

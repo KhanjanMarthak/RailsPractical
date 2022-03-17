2b411b7a16fa2acf84fe67057bec32dc186b6a8c
Khanjan Marthak <khanjan.m@simformsolutions.com>
2022-03-17 16:29:06 +0530

    feat:TE5-TE89, Proper Validations are given to student and faculty model

--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -15,7 +15,7 @@ class FacultiesController < ApplicationController
   def create
     @faculty= Faculty.create(faculty_params)
     if @faculty.valid?
-      redirect_to facultys_path(@faculty)
+      redirect_to faculties_path(@faculty)
     else 
       flash[:errors]= @faculty.errors.full_messages
       render :new
@@ -30,18 +30,18 @@ class FacultiesController < ApplicationController
     puts params
     @faculty= Faculty.find(params[:id])
     @faculty.update(faculty_params)
-    redirect_to facultys_path(@faculty)  
+    redirect_to faculties_path(@faculty)  
   end
     
   def destroy
     @faculty= Faculty.find(params[:id])
     @faculty.destroy
-    redirect_to facultys_path(@faculty)
+    redirect_to faculties_path(@faculty)
   end
   
   private
   def faculty_params
-    params.require(:faculty).permit(:fac_first_name, :fac_last_name, :fac_dob, :email, :phone_number, :desgination )
+    params.require(:faculty).permit(:fac_first_name, :fac_last_name, :fac_dob, :email, :phone_number, :designation )
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
   validates :stud_first_name, :stud_last_name, presence: true
-  validates :stud_dob, comparison: { less_than: Date.today, message: "birthdate can't be in future" }
-  validates :department, inclusion: {in: %w(IT CP ), message: "department can't be %{value}, it has to be IT or CP "}
+  validates :stud_dob, comparison: { less_than: Date.today+1, message: "birthdate can't be in future" }
+  validates :department, inclusion: {in: %w(IT CP), message: "Department can't be %{value}, It has to be IT or CP"}
   validates :term_of_service, acceptance: true, presence: { message: "You cannot proceed without accepting Terms of Usage" }
 end
+ 

109d00429ccc8c6209a514415b105e9761d98d04
Khanjan Marthak <khanjan.m@simformsolutions.com>
2022-03-17 16:29:06 +0530

    feat:TE5-TE89, Proper Validations are given to student and faculty model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,11 +12,13 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student= Student.create(student_params)
+    @student= Student.new(student_params)
+    @student.department = student_params[:department].upcase
+    @student.save
     if @student.valid?
       redirect_to students_path(@student)
     else 
-      flash[:errors]= @student.errors.full_messages
+      flash.now[:errors]= @student.errors.full_messages
       render :new
     end
   end
@@ -29,7 +31,12 @@ class StudentsController < ApplicationController
     puts params
     @student= Student.find(params[:id])
     @student.update(student_params)
-    redirect_to students_path(@student)  
+    if @student.valid?
+      redirect_to students_path(@student)
+    else 
+      flash.now[:errors]= @student.errors.full_messages
+      render :new
+    end  
   end
     
   def destroy
@@ -40,6 +47,6 @@ class StudentsController < ApplicationController
   
   private
   def student_params
-    params.require(:student).permit(:stud_first_name, :stud_last_name, :stud_dob, :term_of_service )
+    params.require(:student).permit(:stud_first_name, :stud_last_name, :stud_dob, :department, :term_of_service )
   end
 end
--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -2,7 +2,7 @@ class Faculty < ApplicationRecord
   validates :fac_first_name, :fac_last_name, presence: true
   validates :fac_dob, comparison: { less_than: Date.today, message: "birthdate can't be in future" }
   validates :email, confirmation:true , format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true
-  #validates :designation, inclusion: {in: %w(Ass.Prof Prof ), message: "designation can't be %{value}, it has to be Asst.Prof or Prof "}
-  #validates :designation, exclusion: {in %w( HOD Sr.Prof ), message: "designation can't be HOD or Sr.Prof "}
-  validates :phone_number, :presence => true, :numericality => true,  :length => { :minimum => 10, :maximum => 10 }
+  validates :designation, inclusion: {in: %w(Asst.Prof Prof), message: "can't be %{value}, it has to be Asst.Prof or Prof "}
+  validates :designation, exclusion: {in: %w(HOD Sr.Prof), message: "designation can't be HOD or Sr.Prof "}
+  validates :phone_number, :presence => true, :numericality => true,  :length => { :minimum => 10, :maximum => 10, message: "Please Write 10 Digit Mobile Number" }
 end

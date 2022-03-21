2b54dc7b3bb2c822a583704f69ab9796fd93fa0a
Khanjan Marthak <khanjan.m@simformsolutions.com>
2022-03-21 17:31:34 +0530

    fix:TE5-TE89, changes suggested by mentor added

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,8 +1,8 @@
 class Faculty < ApplicationRecord
   validates :fac_first_name, :fac_last_name, presence: true
-  validates :fac_dob, comparison: { less_than: Date.today+1, message: "birthdate can't be in future" }
-  validates :email, confirmation:true , format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true
+  validates :fac_dob, comparison: { less_than: Date.today, message: "birthdate can't be in future" }
+  validates :email, confirmation:true , format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true, presence: true
   validates :designation, inclusion: {in: %w(Asst.Prof Prof), message: "can't be %{value}, it has to be Asst.Prof or Prof "}
   validates :designation, exclusion: {in: %w(HOD Sr.Prof), message: "designation can't be HOD or Sr.Prof "}
-  validates :phone_number, :presence => true, :numericality => true,  :length => { :minimum => 10, :maximum => 10, message: "Please Write 10 Digit Mobile Number" }
+  validates :phone_number, :presence => true, :numericality => true,  :length => { :is=>10, message: "should be 10 Digit Mobile Number" }
 end
